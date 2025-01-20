df14aabbe33bfbd64f9e8a745d93734d815ac1d9
Shantelle Toh <shtoh@ucsc.edu>
2025-01-19 20:40:37 -0800

    finish part 10 (Circle class)

--- Assignment 1/src/ColoredPoints.js
+++ Assignment 1/src/ColoredPoints.js
@@ -70,6 +70,7 @@ function connectVariablesToGLSL() {
 // Constants
 const POINT = 0;
 const TRIANGLE = 1;
+const CIRCLE = 2;
 
 let g_selectedColor = [1.0,1.0,1.0,1.0];
 let g_selectedSize = 5;
@@ -84,6 +85,7 @@ function addActionsForHtmlUI() {
 
   document.getElementById('pointButton').onclick = function() { g_selectedType = POINT };
   document.getElementById('triButton').onclick = function() { g_selectedType = TRIANGLE };
+  document.getElementById('circleButton').onclick = function() { g_selectedType = CIRCLE };
 
   // Slider Events
   document.getElementById('redSlide').addEventListener('mouseup', function() { g_selectedColor[0] = this.value / 100; });
@@ -130,8 +132,11 @@ function click(ev) {
   if (g_selectedType == POINT) {
     point = new Point();
   }
-  else {
+  else if (g_selectedType == TRIANGLE) {
     point = new Triangle();
+  }
+  else {
+    point = new Circle();
   }
   point.position = [x, y];
   point.color = g_selectedColor.slice();
